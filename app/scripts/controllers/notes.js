1e6dcd6da31d409601082269182758e7efab94e1
Thirumalaa Srinivas <srinivassekar@gmail.com>
2013-04-14 22:58:02 -0500

    Notes page add and delete functionality.

--- app/scripts/controllers/notes.js
+++ app/scripts/controllers/notes.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('tatooineApp')
-  .controller('NotesCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('NotesCtrl', function ($scope) {
+
+	$scope.notesList = [
+		{ question:'Whats is X?', answer:'none'},
+		{ question:'Whats is Y?', answer:'none'},
+	];
+
+	$scope.addNote = function() {
+		$scope.notesList.push({ question:$scope.newQuestionName, answer:'' });
+		$scope.newQuestionName = "";
+		$scope.newAnswerName = "";
+	}
+
+	$scope.removeNote = function(note) {
+		var meds = $scope.notesList;
+		meds.splice(meds.indexOf(note), 1);
+	}
+
+});
