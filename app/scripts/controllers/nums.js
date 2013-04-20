8a4554a373e6a0ceefc419864a1a0c14f9495ea3
Thirumalaa Srinivas <srinivassekar@gmail.com>
2013-04-19 23:45:20 -0500

    Numbers feature complete.
    vitals and labs added.

--- app/scripts/controllers/nums.js
+++ app/scripts/controllers/nums.js
@@ -6,14 +6,17 @@ angular.module('tatooineApp')
 	$scope.editedNums = null;
 
 	$scope.nums = {
-		weight:171, 
-		heightFeet:5, 
-		heightInches:11, 
-		waist:31
+		weight: 171, 
+		heightFeet: 5, 
+		heightInches: 11, 
+		waist: 31,
+		pulse: 75,
+		bpSys: 99,
+		bpDia: 66,
+		glucose: 90
 	};
 
-	$scope.editNums = function (numType) {
-		
+	$scope.editNums = function (numType) {		
 		$scope.editedNums = numType;
 		console.log($scope.editedNums);
 	};
